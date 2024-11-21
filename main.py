21cbf5240d7147cc41aa099919298bf88ba62720
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-11-21 20:24:05 +0430

    improve input validation and enhance user experience 😎✔👌

--- main.py
+++ main.py
@@ -8,30 +8,58 @@ def main():
         print("[2] Remove guest.")
         print("[3] Show guest list.")
         print("[4] Check if someone is on the list.")
+        print("[5] Exit.")
 
         option = input("Choose an option [1-4]: ")
 
+        # adding gusts
         if option == "1":
             new_gust = input("Enter Gust Full Name to add: ")
             gusts.append(new_gust)
 
+            print(f"{new_gust} added.")
+
+        # removing gusts
         elif option == "2":
             remove_gust = input("Enter Gust Full Name to remove: ")
 
             if remove_gust in gusts:
                 gusts.remove(remove_gust)
+                print(f"{remove_gust} removed!")
+            else:
+                print(f"{remove_gust} not found.")
 
+        # showing gusts list
         elif option == "3":    
-            for gust in gusts:
-                print(gust)
+            if gusts:
+                print("Guest list:")
 
+                for gust in gusts:
+                    print(gust)
+
+            else:
+                print("No guests yet.")
+
+        # searching gust's list
         elif option == "4": 
             find_gust = input("Enter Gust Full Name to find: ")
             if find_gust in gusts:
-                print("Gust Exist.")
+                print(f"{find_gust} is on the list.")
+
+
 
             else:
-                print("Not Registered")
+                print(f"{find_gust} not registered.")
+
+        # exit
+        elif option == "5":
+            print("Goooood bye!")
+            break
+
+
+        else:
+            print("Invalid option, try again.")
+
                 
 
 
