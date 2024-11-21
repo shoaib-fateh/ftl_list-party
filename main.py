5dc9dfacc5df53f50de2014d5eb66ff4161a6412
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-11-21 20:27:25 +0430

    playing with code and adding colors in it 🌈

--- main.py
+++ main.py
@@ -1,74 +1,69 @@
+from colorama import Fore, Back, Style, init
 
+init(autoreset=True)
 
 def main():
     gusts = ["Shoaib", "Fateh", "Omid", "Osman"]
 
     while True:
-        print("[1] Add guest.")
-        print("[2] Remove guest.")
-        print("[3] Show guest list.")
-        print("[4] Check if someone is on the list.")
-        print("[5] Exit.")
+        # options
+        print(Back.CYAN + Fore.BLACK + "[1] Add guest.")
+        print(Back.YELLOW + Fore.BLACK + "[2] Remove guest.")
+        print(Back.GREEN + Fore.BLACK + "[3] Show guest list.")
+        print(Back.MAGENTA + Fore.BLACK + "[4] Check if someone is on the list.")
+        print(Back.RED + Fore.BLACK + "[5] Exit.")
 
-        option = input("Choose an option [1-4]: ")
 
-        # adding gusts
+
+
+        option = input(Fore.BLUE + "Choose an option [1-5]: ")
+
+        # addi gusts
         if option == "1":
-            new_gust = input("Enter Gust Full Name to add: ")
+            new_gust = input(Fore.YELLOW + "Enter Gust Full Name to add: ")
             gusts.append(new_gust)
 
-            print(f"{new_gust} added.")
+            print(Fore.GREEN + f"{new_gust} added.")
 
         # removing gusts
         elif option == "2":
-            remove_gust = input("Enter Gust Full Name to remove: ")
+            remove_gust = input(Fore.YELLOW + "Enter Gust Full Name to remove: ")
 
             if remove_gust in gusts:
                 gusts.remove(remove_gust)
-                print(f"{remove_gust} removed!")
+                print(Fore.RED + f"{remove_gust} removed!")
             else:
-                print(f"{remove_gust} not found.")
+                print(Fore.RED + f"{remove_gust} not found.")
 
         # showing gusts list
         elif option == "3":    
             if gusts:
-                print("Guest list:")
+                print(Fore.CYAN + "Guest list:")
 
                 for gust in gusts:
-                    print(gust)
+                    print(Fore.GREEN + gust)
 
             else:
-                print("No guests yet.")
+                print(Fore.RED + "No guests yet.")
 
         # searching gust's list
         elif option == "4": 
-            find_gust = input("Enter Gust Full Name to find: ")
+            find_gust = input(Fore.YELLOW + "Enter Gust Full Name to find: ")
             if find_gust in gusts:
-                print(f"{find_gust} is on the list.")
-
-
-
+                print(Fore.GREEN + f"{find_gust} is on the list.")
             else:
-                print(f"{find_gust} not registered.")
+                print(Fore.RED + f"{find_gust} not registered.")
 
         # exit
         elif option == "5":
-            print("Goooood bye!")
+            print(Fore.CYAN + "Goooood bye!")
             break
 
-
         else:
-            print("Invalid option, try again.")
-
-                
-
-
-        
+            print(Fore.RED + "Invalid option, try again.")
 
 
 
 
 
 main()
-
-
